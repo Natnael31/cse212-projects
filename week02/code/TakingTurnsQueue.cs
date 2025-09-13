2265e82dd3c9e2da7c8bb809db9114ce114a3871
Natnael31 <end24002byui.edu>
2025-09-13 15:16:47 +0300

    Finished W02 Code: Queues

--- week02/code/TakingTurnsQueue.cs
+++ week02/code/TakingTurnsQueue.cs
@@ -40,15 +40,15 @@ public class TakingTurnsQueue
         else
         {
             Person person = _people.Dequeue();
-            if (person.Turns > 0) // finite turns
+            if (person.Turns > 0)
             {
                 person.Turns -= 1;
                 if (person.Turns > 0)
-                    _people.Enqueue(person); // still has turns left
+                    _people.Enqueue(person);
             }
             else
             {
-                _people.Enqueue(person); // infinite turns
+                _people.Enqueue(person);
             }
             return person;
         }
